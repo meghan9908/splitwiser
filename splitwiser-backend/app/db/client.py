58fa075c79795881b03a1529e835fc852c8a078e
Vraj Patel <vrajpatel4041@gmail.com>
2025-06-17 00:16:21 +0530

    Revert "WIP: client id with firebase google login integrated"
    This reverts commit 47f85318f091314518e0c733d5797f5933bce86e.

--- splitwiser-backend/app/db/client.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGO_URL = getenv("MONGO_URL", "mongodb://localhost:27017")
-client = MongoClient(MONGO_URL)
-db = client["splitwiser"]
